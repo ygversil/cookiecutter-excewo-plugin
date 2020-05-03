911f7b3f218f59568e18ab6d8ac33549f4c2d49c
Yann Voté <ygversil@lilo.org>
2020-05-03 14:18:57 +0200

    Put package docstring on two line to reduce flake8 problems

--- {{cookiecutter.package_slug}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_slug}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,4 +1,6 @@
-"""{{ cookiecutter.package_name }} - {{ cookiecutter.package_description }}"""
+"""{{ cookiecutter.package_name }}
+
+{{ cookiecutter.package_description }}"""
 
 __version__ = '{{ cookiecutter.package_version }}'
 __author__ = '{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>'
